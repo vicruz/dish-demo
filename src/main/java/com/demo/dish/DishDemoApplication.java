d10ea27e3874e8efd398cae996d3905b00eef55f
victor.cruz <victor.cruz@qacg.com>
2021-04-23 23:03:50 -0500

    Project creation

---
+++ src/main/java/com/demo/dish/DishDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/dish/DishDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.dish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
